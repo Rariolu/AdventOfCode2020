26cf7ea0ac39c1b2dc5e9129f3c4eb17231c8080
Rariolu <rcoyneyr82011@gmail.com>
2020-12-03 15:44:57 +0000

    Created function that traverses grid and finds the amount of trees when
    Function produces an incorrect result

--- 3/code.py
+++ 3/code.py
@@ -2,3 +2,27 @@ def CreateGrid():
     f = open("input.txt", "r")
     lines = f.readlines()
     return lines
+
+def FindTrees(grid):
+    x = 3
+    trees = 0
+    print(len(grid))
+    width = len(grid[0])
+    #print(width)
+    #for y in range(1, 34):
+    for y in range(1,len(grid)):
+        tempX = x % len(grid[y])
+        if len(grid[y]) != width:
+            print("width changed on "+str(y)+" to "+str(len(grid[y])))
+        char = grid[y][tempX]
+        tempLine = list(grid[y])
+        if char == '#':
+            trees+=1
+            tempLine[tempX] = 'X'
+        else:
+            tempLine[tempX] = 'O'
+        #print("".join(tempLine))
+        x += 3
+    return trees
+
+print(FindTrees(CreateGrid()))
