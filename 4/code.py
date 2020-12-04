7e1c3de35819f1de2a545894c08f47779d9d4efb
Rariolu <rcoyneyr82011@gmail.com>
2020-12-04 11:52:01 +0000

    Fixed passport parsing and created function that determines "valid" passports

--- 4/code.py
+++ 4/code.py
@@ -5,11 +5,38 @@ def GetPassports():
     passports = []
     for i in range(len(lines)):
         line = lines[i]
-        if line == '\n':
-            currentPassport = currentPassport[:-1]
+        atEnd = i == len(lines)-1
+        if line == '\n' or atEnd:
+            if atEnd:
+                currentPassport += line
+            else:
+                currentPassport = currentPassport[:-1]
             passports.append(currentPassport)
             currentPassport = ""
         else:
             currentPassport += line
     return passports
 
+def GetValidPassports(passports, expectedFields):
+    validPassports = []
+    for i in range(len(passports)):
+        passport = passports[i]
+        fieldsFound = 0
+        missingFields = []
+        for k in range(len(expectedFields)):
+            field = expectedFields[k]
+            if field in passport:
+                fieldsFound += 1
+            else:
+                missingFields.append(field)
+        if fieldsFound == len(expectedFields):
+            validPassports.append(passport)
+        else:
+            print(passport,missingFields,"\n")
+    return validPassports
+
+passports = GetPassports()
+validPassports = GetValidPassports(passports, ["byr","iyr","eyr","hgt","hcl","ecl","pid"])
+"""for i in range(len(validPassports)):
+    print(validPassports[i],"\n")"""
+print(len(validPassports))
