e910c0343e1521b65460f4425aa2f1d302e2b356
Rariolu <rcoyneyr82011@gmail.com>
2020-12-06 16:56:05 +0000

    Created function for parsing input text into "Group" objects and prints the total amount of questions answered affirmatively

--- 6/code.py
+++ 6/code.py
@@ -5,31 +5,38 @@ class Person:
         self.affirmatives.append(question)
 
 class Group:
-    def __init(self):
+    def __init__(self):
         self.people = []
-    def AddPerson(person):
+    def AddPerson(self, person):
         self.people.append(person)
 
 def GetGroups():
     f = open("input.txt","r")
-    """lines = f.readlines()
-    groups = []
-    currentGroup = ""
-    for i in range(len(lines)):
-        line = lines[i]
-        atEnd = i == len(lines)-1
-        if len(line) > 1:
-            currentGroup += line
-        if line == "\n" or atEnd:
-            if currentGroup[len(currentGroup)-1] == "\n":
-                currentGroup = currentGroup[:-1]
-            #print(currentGroup+"\nblep\n")
-            groups.append(currentGroup)
-            currentGroup = ""
-    return groups"""
     groups = f.read().split("\n\n")
     return groups
 
+def ParseGroups(answerGroups):
+    groups = []
+    letters =  list(map(chr, range(97, 123)))
+    totalYes = 0
+    for i in range(len(answerGroups)):
+        group = Group()
+        lines = answerGroups[i].split("\n")
+        answeredQuestions = []
+        for j in range(len(lines)):
+            person = Person()
+            line = lines[j]
+            for k in range(len(line)):
+                char = line[k]
+                if char in letters:
+                    person.AddYes(char)
+                    if char not in answeredQuestions:
+                        answeredQuestions.append(char)
+            group.AddPerson(person)
+        groups.append(group)
+        totalYes += len(answeredQuestions)
+    print("Total Affirmatives:",totalYes)
+    return groups
+
 groupAnswers = GetGroups()
-for i in range(len(groupAnswers)):
-    print(groupAnswers[i]+"\n")
+groups = ParseGroups(groupAnswers)
