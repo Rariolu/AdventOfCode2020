ce400d17b9f1f4a354297eafdd0d17ba80ab516e
Rariolu <rcoyneyr82011@gmail.com>
2020-12-06 16:38:49 +0000

    Created function which splits input text into separate groups of people

--- 6/code.py
+++ 6/code.py
@@ -10,3 +10,26 @@ class Group:
     def AddPerson(person):
         self.people.append(person)
 
+def GetGroups():
+    f = open("input.txt","r")
+    """lines = f.readlines()
+    groups = []
+    currentGroup = ""
+    for i in range(len(lines)):
+        line = lines[i]
+        atEnd = i == len(lines)-1
+        if len(line) > 1:
+            currentGroup += line
+        if line == "\n" or atEnd:
+            if currentGroup[len(currentGroup)-1] == "\n":
+                currentGroup = currentGroup[:-1]
+            #print(currentGroup+"\nblep\n")
+            groups.append(currentGroup)
+            currentGroup = ""
+    return groups"""
+    groups = f.read().split("\n\n")
+    return groups
+
+groupAnswers = GetGroups()
+for i in range(len(groupAnswers)):
+    print(groupAnswers[i]+"\n")
