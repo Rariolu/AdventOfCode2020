96091963aaed90eede2f4866e62e76aa094e65e4
Rariolu <rcoyneyr82011@gmail.com>
2020-12-06 16:22:07 +0000

    Created classes for managing people and groups of people

---
+++ 6/code.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self):
+        self.affirmatives = []
+    def AddYes(self, question):
+        self.affirmatives.append(question)
+
+class Group:
+    def __init(self):
+        self.people = []
+    def AddPerson(person):
+        self.people.append(person)
+
